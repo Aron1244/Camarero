fcefb71eb4fa311a0054390349c1e2d12c8f2aae
Benjamin Herrera <raycuasa80@gmail.com>
2025-04-25 20:03:49 -0400

    add view comanda fix styles components/view

--- src/components/widgetCloudinary.jsx
+++ src/components/widgetCloudinary.jsx
@@ -6,7 +6,6 @@ const WidgetCloudinary = ({ onSuccess }) => {
   useEffect(() => {
     const script = document.createElement("script");
     script.src = "https://upload-widget.cloudinary.com/latest/global/all.js";
-    script.type = "text/javascript";
     script.async = true;
     document.body.appendChild(script);
 
@@ -15,6 +14,25 @@ const WidgetCloudinary = ({ onSuccess }) => {
         {
           cloudName: "dovt9safa",
           uploadPreset: "camarero",
+          sources: ["local", "url", "camera"],
+          multiple: false,
+          cropping: false,
+          maxFileSize: 1500000,
+          clientAllowedFormats: ["jpg", "jpeg", "png"],
+          styles: {
+            palette: {
+              window: "#ffffff",
+              sourceBg: "#f4f4f4",
+              windowBorder: "#90a0b3",
+              tabIcon: "#000000",
+              link: "#0078ff",
+              action: "#0078ff",
+              complete: "#20B832",
+              error: "#c43737",
+              textDark: "#000000",
+              textLight: "#ffffff",
+            },
+          },
         },
         (error, result) => {
           if (!error && result && result.event === "success") {
@@ -23,13 +41,9 @@ const WidgetCloudinary = ({ onSuccess }) => {
         }
       );
 
-      document.getElementById("upload_widget").addEventListener(
-        "click",
-        function () {
-          myWidget.open();
-        },
-        false
-      );
+      document
+        .getElementById("upload_widget")
+        .addEventListener("click", () => myWidget.open());
     };
 
     return () => {
@@ -38,7 +52,25 @@ const WidgetCloudinary = ({ onSuccess }) => {
   }, [onSuccess]);
 
   return (
-    <button id="upload_widget" className="cloudinary-button">
+    <button
+      id="upload_widget"
+      className="w-full md:w-auto px-4 py-2 bg-blue-600 text-white text-sm rounded hover:bg-blue-700 transition flex space-between gap-2"
+    >
+      <svg
+        className="w-6 h-6 text-gray-800 dark:text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fillRule="evenodd"
+          d="M7.5 4.586A2 2 0 0 1 8.914 4h6.172a2 2 0 0 1 1.414.586L17.914 6H19a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h1.086L7.5 4.586ZM10 12a2 2 0 1 1 4 0 2 2 0 0 1-4 0Zm2-4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Z"
+          clipRule="evenodd"
+        />
+      </svg>
       Subir Imagen
     </button>
   );
